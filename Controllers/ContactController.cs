cf7d493af2d95d8817a05aa0bd6cdd9146437909
Jacob Erlandsson <erlandssonjacob@hotmail.com>
2023-10-02 14:28:24 +0200

    forms

---
+++ Controllers/ContactController.cs
@@ -0,0 +1,38 @@
+﻿using Critoo.Models;
+using Critoo.Services;
+using MailKit;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace Critoo.Controllers
+{
+    public class ContactController : SurfaceController
+    {
+        public ContactController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+        }
+
+        [HttpPost]
+        public IActionResult Index(ContactForm contactForm)
+        {
+            if(!ModelState.IsValid)
+                return CurrentUmbracoPage();
+
+            using var email = new MailSenderService("no-reply@crito.com", "smtp.crito.com", 587, "contact@crito.com", "BytMig123!");
+
+            email.SendEmailAsync(contactForm.Email, "We have received your message", "We have received your message and will get back to you asap").ConfigureAwait(false);
+            email.SendEmailAsync("umbraco@crito.com", $"{ contactForm.Name} sent a message", contactForm.Message ).ConfigureAwait(false);
+
+            return LocalRedirect(contactForm.RedirectURL ?? "/");
+
+
+            
+        }
+    }
+}
---
+++ Services/MailSenderService.cs
@@ -0,0 +1,62 @@
+﻿using MailKit.Net.Smtp;
+using MimeKit;
+
+namespace Critoo.Services
+{
+    public class MailSenderService : IDisposable
+    {
+
+        private string _mailFrom;
+        private string _smtp;
+        private int _port;
+        private string _username;
+        private string _password;
+
+        private MailKit.Net.Smtp.SmtpClient _client;
+        public MailSenderService(string mailFrom, string smtp, int port, string username, string password)
+        {
+            _mailFrom = mailFrom;
+            _smtp = smtp;
+            _port = port;
+            _username = username;
+            _password = password;
+
+            _client = new MailKit.Net.Smtp.SmtpClient();
+        }
+
+        public async Task SendEmailAsync (string to, string subject, string body)
+        {
+            try
+            {
+                var email = new MimeMessage();
+                email.From.Add(MailboxAddress.Parse(_mailFrom));
+                email.To.Add(MailboxAddress.Parse(to));
+                email.Subject = subject;
+                email.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = body };
+
+                using var smtpClient = new SmtpClient();
+                await _client.ConnectAsync(_smtp, _port, MailKit.Security.SecureSocketOptions.StartTls);
+                await _client.AuthenticateAsync(_username, _password);
+
+                var result = await _client.SendAsync(email);
+            }
+            catch
+            {   }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if(disposing)
+            {
+                _client.DisconnectAsync(true).ConfigureAwait(false);
+            }
+
+        }
+    }
+}
